af1742835b5db1b048b1fa66f6a8ab4e6e19e7d0
Andrew T <andrew.t@adaas.org>
2024-07-23 22:18:26 +0300

    new version created ::  0.0.23

--- dist/src/global/A_EXPRESS_ServerCommandsController.class.js
+++ dist/src/global/A_EXPRESS_ServerCommandsController.class.js
@@ -3,6 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.A_EXPRESS_ServerCommandsController = void 0;
 const A_EXPRESS_EntityController_class_1 = require("./A_EXPRESS_EntityController.class");
 class A_EXPRESS_ServerCommandsController extends A_EXPRESS_EntityController_class_1.A_EXPRESS_EntityController {
+    constructor(config) {
+        super(config);
+    }
 }
 exports.A_EXPRESS_ServerCommandsController = A_EXPRESS_ServerCommandsController;
 //# sourceMappingURL=A_EXPRESS_ServerCommandsController.class.js.map
--- src/global/A_EXPRESS_ServerDelegateController.class.ts
+++ src/global/A_EXPRESS_ServerDelegateController.class.ts
@@ -1,5 +1,5 @@
-import { A_SDK_TYPES__Dictionary } from "@adaas/a-sdk-types";
-import { A_EXPRESS_TYPES__IControllerRepository } from "../types/A_EXPRESS_EntityController.types";
+import { A_SDK_TYPES__DeepPartial, A_SDK_TYPES__Dictionary } from "@adaas/a-sdk-types";
+import { A_EXPRESS_TYPES__EntityControllerConfig, A_EXPRESS_TYPES__IControllerRepository } from "../types/A_EXPRESS_EntityController.types";
 import { A_EXPRESS_EntityController } from "./A_EXPRESS_EntityController.class";
 import { A_EXPRESS_TYPES__SERVER_DELEGATE_IRequest, } from "../types/A_EXPRESS_ServerDelegateController.types";
 
@@ -9,11 +9,19 @@ import { A_EXPRESS_TYPES__SERVER_DELEGATE_IRequest, } from "../types/A_EXPRESS_S
 export class A_EXPRESS_ServerDelegateController<
     _DBEntityType extends A_SDK_TYPES__Dictionary<any> = A_SDK_TYPES__Dictionary<any>,
     _RepositoryType extends A_EXPRESS_TYPES__IControllerRepository<_DBEntityType> = A_EXPRESS_TYPES__IControllerRepository<_DBEntityType>,
+    _RequestType extends A_EXPRESS_TYPES__SERVER_DELEGATE_IRequest = A_EXPRESS_TYPES__SERVER_DELEGATE_IRequest
 > extends A_EXPRESS_EntityController<
     A_EXPRESS_TYPES__SERVER_DELEGATE_IRequest,
     _DBEntityType,
     _RepositoryType
 > {
 
+
+
+    constructor(
+        config?: A_SDK_TYPES__DeepPartial<A_EXPRESS_TYPES__EntityControllerConfig<_DBEntityType, _RequestType>>
+    ) {
+        super(config);
+    }
 
 }
