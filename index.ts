66eebbc4eb7278fee3d166c1cfc155601d63366b
Andrew T <andrew.t@adaas.org>
2024-07-23 18:59:09 +0300

    new version created ::  0.0.21

--- index.ts
+++ index.ts
@@ -41,12 +41,41 @@ export {
     A_EXPRESS_TYPES__HealthControllerConfig
 } from './src/types/A_EXPRESS_HealthRouter.types'
 
+export {
+    A_EXPRESS_TYPES__APP_INTERACTIONS_ControllerConfig,
+    A_EXPRESS_TYPES__APP_INTERACTIONS_IRequest,
+    A_EXPRESS_TYPES__APP_INTERACTIONS_IRequestParams,
+    A_EXPRESS_TYPES__APP_INTERACTIONS_IRequestQueryParams,
+    A_EXPRESS_TYPES__APP_INTERACTIONS_IResponse
+} from './src/types/A_EXPRESS_AppInteractionsController.types';
+
+export {
+    A_EXPRESS_TYPES__SERVER_COMMANDS_ControllerConfig,
+    A_EXPRESS_TYPES__SERVER_COMMANDS_IRequest,
+    A_EXPRESS_TYPES__SERVER_COMMANDS_IRequestParams,
+    A_EXPRESS_TYPES__SERVER_COMMANDS_IRequestQueryParams,
+    A_EXPRESS_TYPES__SERVER_COMMANDS_IResponse
+} from './src/types/A_EXPRESS_ServerCommandsController.types';
+
+export {
+    A_EXPRESS_TYPES__SERVER_DELEGATE_ControllerConfig,
+    A_EXPRESS_TYPES__SERVER_DELEGATE_IRequest,
+    A_EXPRESS_TYPES__SERVER_DELEGATE_IRequestParams,
+    A_EXPRESS_TYPES__SERVER_DELEGATE_IRequestQueryParams,
+    A_EXPRESS_TYPES__SERVER_DELEGATE_IResponse
+} from './src/types/A_EXPRESS_ServerDelegateController.types';
+
+
+
 
 // =========== DEFAULT ENTITIES EXPORT
 export { A_EXPRESS_AuthController } from './src/global/A_EXPRESS_AuthController.class';
 export { A_EXPRESS_Controller } from './src/global/A_EXPRESS_Controller.class';
 export { A_EXPRESS_EntityController } from './src/global/A_EXPRESS_EntityController.class';
 export { A_EXPRESS_HealthController } from './src/global/A_EXPRESS_HealthRouter.class';
+export { A_EXPRESS_AppInteractionsController } from './src/global/A_EXPRESS_AppInteractionsController.class';
+export { A_EXPRESS_ServerCommandsController } from './src/global/A_EXPRESS_ServerCommandsController.class';
+export { A_EXPRESS_ServerDelegateController } from './src/global/A_EXPRESS_ServerDelegateController.class';
 
 // =========== MIDDLEWARES EXPORT
 export { A_EXPRESS_AuthMiddleware } from './src/middleware/A_EXPRESS_Auth.middleware';
